827abb7b83aaf33441598d3bc2e15cf179221224
David <davidpkelly@me.com>
2022-03-12 14:03:19 +0000

    Add stats to modal from localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,15 @@
 
 const btnNum = document.getElementById("btn-num");
 const lastAnswerDisplay = document.getElementById("lastAnswerDisplay");
+const statsGamesPlayedDisplay = document.getElementById("games-played-display");
 const highScoreDisplay = document.getElementById("high-score-display");
+const highScoreDateDisplay = document.getElementById("high-score-date-display");
+const highScoreHardModeDisplay = document.getElementById("high-score-hard-display");
+const highScoreHardModeDateDisplay = document.getElementById("high-score-hard-date-display");
 
 const gameState = {
     gameOver: false,
+    hardMode: false,
     currentScore: 0,
     currentNum: 1,
 }
@@ -29,149 +34,154 @@ function handleGameOver() {
     lastAnswerDisplay.innerHTML = `Game Over! Your score was ${gameState.currentScore}.`;
     lastAnswerDisplay.style.backgroundColor = "rgba(249, 140, 141, 1)";
 
+    updateGamesPlayed();
     logHighScore();
 
     // Stats modal appears after 3 seconds
-    setTimeout(() => {modals[1].style.display = "block";}, 3000)
+    // setTimeout(() => {
+    //     modals[1].style.display = "block";
+    // }, 3000)
 
     handleGameReset();
 }
 
+function updateGamesPlayed() {
+    let gamesPlayed = localStorage.getItem("gamesPlayed");
+    gamesPlayed ? gamesPlayed = parseInt(gamesPlayed) : localStorage.setItem("gamesPlayed", 0);
+    gamesPlayed += 1;
+    statsGamesPlayedDisplay.innerHTML = gamesPlayed;
+    localStorage.setItem("gamesPlayed", gamesPlayed);
+}
+
 //** Logs High Score */
 function logHighScore() {
+    let finalScore = gameState.currentScore;
+    let today = new Date();
 
-    // let today = new Date();
-    let finalScore = gameState.currentScore
-    let savedHighScore = window.localStorage.getItem("highscore");
+    if (!gameState.hardMode) {
+        if (finalScore > localStorage.getItem("highscore")) {
+            localStorage.setItem("highscore", finalScore);
+            localStorage.setItem("highscoreDate", today.toDateString());
+        }
+     }
+    else if (finalScore > localStorage.getItem("highscoreHardMode")) {
+            localStorage.setItem("highscoreHardMode", finalScore);
+            localStorage.setItem("highscoreDateHardMode", today.toDateString());
+        }
 
-    // const scoreTableEntry = {
-    //     score: gameState.currentScore,
-    //     dateRecorded: {
-    //         date: today.getDate(),
-    //         month: today.getMonth(),
-    //         year: today.getFullYear()
-    //     }
-    // }
-
-    // window.localStorage.setItem("highscore", JSON.stringify(scoreTableEntry));
-
-    // JSON.parse(window.localStorage.getItem("highscore"))
-
-    if (finalScore > savedHighScore) {
-        savedHighScore = gameState.currentScore;
+        highScoreDisplay.innerHTML = `${localStorage.getItem("highscore")}`;
+        highScoreDateDisplay.innerHTML = `${localStorage.getItem("highscoreDate")}`
+        highScoreHardModeDisplay.innerHTML = `${localStorage.getItem("highscoreHardMode")}`;
+        highScoreHardModeDateDisplay.innerHTML = `${localStorage.getItem("highscoreDateHardMode")}`;
     }
 
-    highScoreDisplay.innerHTML = savedHighScore;
-
-}
-
-/** Resets the game */
-function handleGameReset() {
-    gameState.gameOver = false;
-    gameState.currentNum = 1;
-    gameState.currentScore = 0;
-    updateNumBtn()
-}
-
-/** Updates the number on the button */
-function updateNumBtn() {
-    btnNum.innerText = gameState.currentNum;
-}
-
-// Tests
-// console.log(`Check 3: Expected result is FIZZ, actual result is ${checkNumber(3)} `)
-// console.log(`Check 5: Expected result is BUZZ, actual result is ${checkNumber(5)} `)
-// console.log(`Check 7: Expected result is 7, actual result is ${checkNumber(7)} `)
-// console.log(`Check 15: Expected result is FIZZBUZZ, actual result is ${checkNumber(15)} `)
-// console.log(`Check "string": Expected result is TypeError, actual result is ${checkNumber("string")} `)
-
-/** Handles clicks on the game buttons */
-function handleClick() {
-    if (this.dataset.gameBtn === "number") {
-        handleInput(gameState.currentNum);
-    } else {
-        handleInput(this.dataset.gameBtn);
-    }
-}
-
-/** Handles arrow key presses */
-function handleKeyPress(event) {
-    switch (event.code) {
-        case "ArrowLeft":
-            handleInput("fizz");
-            break;
-        case "ArrowRight":
-            handleInput("buzz");
-            break;
-        case "ArrowUp":
-            handleInput(gameState.currentNum);
-            break;
-        case "ArrowDown":
-            handleInput("fizzbuzz");
-            break;
-        default:
-            return
-    }
-}
-
-/** Checks if the input matches the expected result */
-function isInputCorrect(input) {
-    if (input === checkNumber(gameState.currentNum)) {
-        return true;
-    } else return false;
-}
-
-/** Handles game input */
-function handleInput(input) {
-    console.log(`checking input ${input} against answer ${checkNumber(gameState.currentNum)}`)
-
-    if (isInputCorrect(input)) {
-        gameState.currentNum += 1;
-        gameState.currentScore += 1;
+    /** Resets the game */
+    function handleGameReset() {
+        gameState.gameOver = false;
+        gameState.currentNum = 1;
+        gameState.currentScore = 0;
         updateNumBtn()
-        lastAnswerDisplay.innerHTML = "👍";
-    } else handleGameOver();
-}
+    }
 
-/** Add click event listeners for game buttons and key presses */
-const gameButtons = document.getElementsByClassName("btn-game");
+    /** Updates the number on the button */
+    function updateNumBtn() {
+        btnNum.innerText = gameState.currentNum;
+    }
 
-for (button of gameButtons) {
-    button.addEventListener("click", handleClick);
-}
+    // Tests
+    // console.log(`Check 3: Expected result is FIZZ, actual result is ${checkNumber(3)} `)
+    // console.log(`Check 5: Expected result is BUZZ, actual result is ${checkNumber(5)} `)
+    // console.log(`Check 7: Expected result is 7, actual result is ${checkNumber(7)} `)
+    // console.log(`Check 15: Expected result is FIZZBUZZ, actual result is ${checkNumber(15)} `)
+    // console.log(`Check "string": Expected result is TypeError, actual result is ${checkNumber("string")} `)
 
-document.addEventListener("keydown", handleKeyPress);
-
-// Modals
-
-// Get the modal
-const navItems = document.getElementsByClassName("nav-item");
-const modals = document.getElementsByClassName("modal");
-const closeSpans = document.getElementsByClassName("close");
-
-for (let navItem of navItems) {
-    navItem.addEventListener("click", launchModal)
-}
-
-function launchModal() {
-    let linkClicked = this.dataset.link;
-    modals[this.dataset.link].style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-for (let closeSpan of closeSpans) {
-    closeSpan.addEventListener("click", () => {
-        for (let modal of modals) {
-            modal.style.display = "none";
+    /** Handles clicks on the game buttons */
+    function handleClick() {
+        if (this.dataset.gameBtn === "number") {
+            handleInput(gameState.currentNum);
+        } else {
+            handleInput(this.dataset.gameBtn);
         }
-    })
-}
+    }
 
-// Close modal when the user clicks anywhere outside of the modal
-for (let modal of modals) {
-    modal.addEventListener("click", (e) => {
-        if (e.target.classList.contains("modal")) {
-            modal.style.display = "none";
+    /** Handles arrow key presses */
+    function handleKeyPress(event) {
+        switch (event.code) {
+            case "ArrowLeft":
+                handleInput("fizz");
+                break;
+            case "ArrowRight":
+                handleInput("buzz");
+                break;
+            case "ArrowUp":
+                handleInput(gameState.currentNum);
+                break;
+            case "ArrowDown":
+                handleInput("fizzbuzz");
+                break;
+            default:
+                return
         }
-    })
-}
+    }
+
+    /** Checks if the input matches the expected result */
+    function isInputCorrect(input) {
+        if (input === checkNumber(gameState.currentNum)) {
+            return true;
+        } else return false;
+    }
+
+    /** Handles game input */
+    function handleInput(input) {
+        console.log(`checking input ${input} against answer ${checkNumber(gameState.currentNum)}`)
+
+        if (isInputCorrect(input)) {
+            gameState.currentNum += 1;
+            gameState.currentScore += 1;
+            updateNumBtn()
+            lastAnswerDisplay.innerHTML = "👍";
+        } else handleGameOver();
+    }
+
+    /** Add click event listeners for game buttons and key presses */
+    const gameButtons = document.getElementsByClassName("btn-game");
+
+    for (button of gameButtons) {
+        button.addEventListener("click", handleClick);
+    }
+
+    document.addEventListener("keydown", handleKeyPress);
+
+    // Modals
+
+    // Get the modal
+    const navItems = document.getElementsByClassName("nav-item");
+    const modals = document.getElementsByClassName("modal");
+    const closeSpans = document.getElementsByClassName("close");
+
+    for (let navItem of navItems) {
+        navItem.addEventListener("click", launchModal)
+    }
+
+    function launchModal() {
+        let linkClicked = this.dataset.link;
+        modals[this.dataset.link].style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    for (let closeSpan of closeSpans) {
+        closeSpan.addEventListener("click", () => {
+            for (let modal of modals) {
+                modal.style.display = "none";
+            }
+        })
+    }
+
+    // Close modal when the user clicks anywhere outside of the modal
+    for (let modal of modals) {
+        modal.addEventListener("click", (e) => {
+            if (e.target.classList.contains("modal")) {
+                modal.style.display = "none";
+            }
+        })
+    }
